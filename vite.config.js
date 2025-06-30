0cb113dd662f9fd03f06a0de0e9314d62bb790a7
Andre Zeni <AFSZENI@GMAIL.COM>
2025-06-30 16:51:15 +0200

    Fix: Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/andre-zeni-portfolio/', // Add this line
+  plugins: [react( ), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
